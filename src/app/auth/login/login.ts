6d47c09e1923f0e218a9e63e10c7d52c3c6a50d2
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-12-07 13:32:14 -0500

    Implement form validation and api integration

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -5,6 +5,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AxiosResponse } from 'axios';
+import { Api } from '../../core/services/api';
 
 @Component({
   selector: 'app-login',
@@ -15,15 +18,35 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatIconModule,
     MatButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  constructor(private apiService: Api) {}
+
   hidePassword = signal(true);
+
+  readonly loginForm = new FormGroup({
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    clave: new FormControl('', [Validators.required]),
+  });
 
   togglePassword(event: MouseEvent) {
     this.hidePassword.set(!this.hidePassword());
     event.stopPropagation();
+  }
+
+  async onSubmit() {
+    const loginResponse: AxiosResponse = await this.apiService.post(
+      '/usuario/login',
+      this.loginForm.getRawValue(),
+    );
+
+    if (loginResponse.status === 200) {
+      console.log(loginResponse);
+      console.log('REDIRECT TO DASHBOARD');
+    }
   }
 }
