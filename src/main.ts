ea27d715eeaf9306387a6798eee96169cfc987a6
Sylbird <84147332+Sylbird@users.noreply.github.com>
2026-01-01 21:57:33 -0500

    Add Spanish (Peru) locale support

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-PE';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+// Register Spanish (Peru) locale
+registerLocaleData(localeEs);
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
