ab431cb9b6c5c50bd9159341aae607335edf36b9
Sylbird <84147332+Sylbird@users.noreply.github.com>
2026-04-03 01:04:26 -0500

    Update production API URL to include api path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://backend-sistema-mototaxis.onrender.com',
+  API_URL: 'https://backend-sistema-mototaxis.onrender.com/api/v1',
 };
